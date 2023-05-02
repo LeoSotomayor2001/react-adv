d6483462d73497b0b3350fbf3a45d54698b7cb66
Leodomi <Leodomi15@gmail.com>
2023-05-01 21:05:18 -0400

    control props alt

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Product, ProductInCart } from "../interfaces/interfaces";
-import { products } from '../data/products';
+
 
 
 
@@ -12,38 +12,22 @@ export const useShoppingCart=()=>{
     // console.log('onProductCountChange');
     setShoppingCart(oldShoppingCart => {
 
-      const productInCart: ProductInCart=oldShoppingCart[product.id] || {...product,count:0};
+      if(count===0){
 
-      if(Math.max(productInCart.count+ count,0) > 0){
-        productInCart.count+=count;
-        return{
-          ...oldShoppingCart ,
-          [productInCart.id]: productInCart
-        }
-      }
-
-      
         const {[product.id]:toDelete,...rest}=oldShoppingCart;
 
-        return rest;
+        return{...rest}
 
-      // if(count===0){
+      }
 
-      //   const {[product.id]:toDelete,...rest}=oldShoppingCart;
-
-      //   return{...rest}
-
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]:{...product,count}
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]:{...product,count}
+      };
     })
   }
 
   return{
-    products,
     shoppingCart,
     onProductCountChange
   }
